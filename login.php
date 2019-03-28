d4724371d1d749f3ba5ee1e68cb8a2334fde30c9
imene <imenemaghrebi@gmail.com>
2019-03-28 19:24:40 +0100

    login page

--- login.php
+++ login.php
@@ -5,24 +5,43 @@
     <LINK rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    <h1>Login</h1> <input type="submit" name="login" value="Submit">
-    <div class="control-group">
-        <form name="login" method="post" action="index.php?page=admin">
-            <p>
-                <label>Username</label>
-                <input name="username" type="text" maxlength=30 value="name">
-            </p>
-            <p>
-                <label>Password</label>
-                <input name="password" type="password" maxlength=30>
-            </p>
-            <p>
-                <button type="submit" name="login" value="Submit">Submit</button>
-            </p>
-        </form>
+<body>
+<div class="container">
+    <div class="row">
+        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div class="card card-signin my-5">
+                <div class="card-body">
+                    <h5 class="card-title text-center">Sign In</h5>
+                    <form class="form-signin">
+                        <div class="form-label-group">
+                            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" >
+                            <label for="inputEmail">Email address</label>
+                            <p class="help-block text-danger"></p>
+                        </div>
+
+                        <div class="form-label-group">
+                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                            <label for="inputPassword">Password</label>
+                            <p class="help-block text-danger"></p>
+                        </div>
+
+                        <div class="custom-control custom-checkbox mb-3">
+                            <input type="checkbox" class="custom-control-input" id="customCheck1">
+                            <label class="custom-control-label" for="customCheck1">Remember password</label>
+                            <p class="help-block text-danger"></p>
+                        </div>
+                        <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
+</div>
 </body>
 </html>
+
+
+
 
 
 <?php
