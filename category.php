f9a538ac18fa6404df70c054643a059bb79cf300
imene <imenemaghrebi@gmail.com>
2019-03-29 22:06:28 +0100

    Article display

--- category.php
+++ category.php
@@ -35,23 +35,62 @@
     if (mysqli_num_rows($article_query)==0) {
         echo "no article in the database";
     } else {
-        do {
-            ?>
+      ?>
+
+            <div class="offset-1 col">
+
+                <?php
+                do {
+                    ?>
+                    <div class="row " style ="width: 80%; border: 2px solid #C0C0C0; border-radius: 5px;">
+
+                        <div class="col-3" >
+                            <img  src="images/<?php echo $article_res['img']; ?>" style="width:100%; padding-top: 60px;  "alt="Card image cap">
+                        </div>
+                        <div class="col" style="padding-bottom: 10px; padding-top: 10px; ">
+                            <div  style="padding-left:20px; ">
+                                <h5 style="color: 	#787878"> <?php echo $article_res['title']; ?> </h5>
+                                <hr style="border: 0.5px solid 	#C0C0C0">
+                            </div>
+                            <div style="padding-left: 20px; ">
+                                <p  style="line-height:1.2em; height:2.4em; overflow:hidden;"> <?php echo $article_res['text']; ?> </p>
+                                <p style="text-align: center">  ... </p>
+
+
+                                <hr style="border: 0.5px solid 	#C0C0C0">
+                            </div>
+
+                            <div style="padding-left: 20px; text-align: center; ">
+                                <a  style="  margin:0 auto;" href="index.php?page=item&articleID=<?php echo $article_res['id']; ?>" class="btn btn-primary">View More </a>
+                            </div>
+                        </div>
+                    </div>
+                    <br/>
+
+                    <?php
+                }while ($article_res = mysqli_fetch_assoc($article_query));
+                ?>
+
+            </div>
+
+
+
+           <!--
             <div class="item">
-                <a href="index.php?page=item&articleID=<?php echo $article_res['id']; ?>">
+                <a href="index.php?page=item&articleID=<?php /*echo $article_res['id']; */?>">
                     <p>
-                        <h2><?php echo $article_res['title']; ?></h2>
+                        <h2><?php /*echo $article_res['title']; */?></h2>
                     </p>
                 </a>
 
                 <p>
-                    <img src="images/<?php echo $article_res['img']; ?>" style="width:200px;height:150px;">
+                    <img src="images/<?php /*echo $article_res['img']; */?>" style="width:200px;height:150px;">
 
                 </p>
             </div>
-        <?php
-        } while ($article_res=mysqli_fetch_assoc($article_query));
-        ?>
+        --><?php
+/*        } while ($article_res=mysqli_fetch_assoc($article_query));
+        */?>
     <?php
     }  ?>
 
--- home.php
+++ home.php
@@ -11,7 +11,7 @@
         </div>
     </div>
 </header>
-<h1> this is our home page</h1>
+
 <?php
 //select all articles belonging to the selected categoryID
 $article_sql="SELECT article.id, article.title, article.img, article.text
@@ -22,24 +22,40 @@ if ($article_query=mysqli_query($dbconnect, $article_sql)){
 if (mysqli_num_rows($article_query)==0) {
     echo "no article in the database";
 } else {
-   do {
-        ?>
-        <div class="item">
-            <p>
-                <a href="index.php?page=item&articleID=<?php echo $article_res['id']; ?>">
-                <h2>
-                <?php echo $article_res['title']; ?>
-                </h2>
-                </a>
-            </p>
-            <p>
-                <img src="images/<?php echo $article_res['img']; ?>" style="width:200px;height:150px;">
-
-            </p>
-
-        </div>
-        <?php
-    } while ($article_res=mysqli_fetch_assoc($article_query));
     ?>
-    <?php
-}
+
+    <div class="offset-1 col">
+
+        <?php
+        do {
+            ?>   <div class="row " style ="width: 80%; border: 2px solid #C0C0C0; border-radius: 5px;">
+
+                        <div class="col-3" >
+                            <img  src="images/<?php echo $article_res['img']; ?>" style="width:100%; padding-top: 60px;  "alt="Card image cap">
+                        </div>
+                        <div class="col" style="padding-bottom: 10px; padding-top: 10px; ">
+                            <div  style="padding-left:20px; ">
+                                <h5 style="color: 	#787878"> <?php echo $article_res['title']; ?> </h5>
+                                <hr style="border: 0.5px solid 	#C0C0C0">
+                            </div>
+                            <div style="padding-left: 20px; ">
+                                <p  style="line-height:1.2em; height:2.4em; overflow:hidden;"> <?php echo $article_res['text']; ?> ... </p>
+                                <p style="text-align: center">  ... </p>
+                                <hr style="border: 0.5px solid 	#C0C0C0">
+                            </div>
+
+                            <div style="padding-left: 20px; text-align: center; ">
+                                 <a  style="  margin:0 auto;" href="index.php?page=item&articleID=<?php echo $article_res['id']; ?>" class="btn btn-primary">View More </a>
+                            </div>
+                        </div>
+                 </div>
+                 <br/>
+
+            <?php
+        }while ($article_res = mysqli_fetch_assoc($article_query));
+        ?>
+
+    </div>
+<?php
+}?>
+
